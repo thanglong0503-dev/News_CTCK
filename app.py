4e5eb978d49798dfd01f40955119fb1b15a4d783
thanglong0503-dev <thanglong0503@gmail.com>
2026-03-29 20:14:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from frontend.views import render_home_page
 
 # Cấu hình trang (Phải đặt ở đầu file)
 st.set_page_config(
-    page_title="News CTCK | Research",
+    page_title="Vietnam Securities Research", # Đổi tên Tab trình duyệt
+    page_icon="assets/logo.png",              # Gọi logo Linance làm icon
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="expanded"
 )
 
 # Chạy trang chủ
